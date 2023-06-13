239a36b5593181f13a75b9f657b06954088260c1
sholealirezaei <sh.alirezaii@gmail.com>
2023-06-13 12:30:14 +0330

    Update script.js

--- Weather/script.js
+++ Weather/script.js
@@ -14,7 +14,7 @@ let weather = {
         const {icon, description} = data.weather[0];
         const {temp, humidity} = data.main;
         const {speed} = data.wind;
-        console.log(name, icon, description, temp, humidity, speed);
+        console.log(data);
         document.querySelector(".city").innerText = "Weather in " + name;
         document.querySelector(".icon").src = "https://openweathermap.org/img/wn/"+ icon +"@2x.png"
         document.querySelector(".description").innerText = description;
